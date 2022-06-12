8c05e7ef93a3d454ae2f004de1d765c1acad1e11
ivan7922 <mvrom@mail.ru>
2022-06-12 14:20:21 +0300

    12.06.22

---
+++ task_1/js/task_1.js
@@ -0,0 +1,60 @@
+// исходный XML файл
+
+const xmlStr= `
+    <list>
+    <student>
+        <name lang="en">
+        <first>Ivan</first>
+        <second>Ivanov</second>
+        </name>
+        <age>35</age>
+        <prof>teacher</prof>
+    </student>
+    <student>
+        <name lang="ru">
+        <first>Петр</first>
+        <second>Петров</second>
+        </name>
+        <age>58</age>
+        <prof>driver</prof>
+    </student>
+    </list>`;
+
+// парсим XML
+
+const parser = new DOMParser();
+const xmlDom = parser.parseFromString(xmlStr, "text/xml");
+
+// функция создания массива объектов "студент"
+
+const studentObj = {};
+let j=0;
+
+function getStudent(obj){
+  const listNode = obj.querySelector('list');
+  for (const studentNode of listNode.querySelectorAll('student')) {
+    const nameNode = studentNode.querySelector('name');
+    const firstNameNode = nameNode.querySelector('first');
+    const secondNameNode = nameNode.querySelector('second');
+    const ageNode = studentNode.querySelector('age');
+    const profNode = studentNode.querySelector('prof');
+    const langAttr = nameNode.getAttribute('lang');
+    studentObj[j] = [
+      { name : firstNameNode.textContent +" "+ secondNameNode.textContent, age : Number(ageNode.textContent), prof : profNode.textContent, lang : langAttr },
+    ]; 
+    j++;      
+  }      
+  return studentObj;
+}
+
+getStudent(xmlDom);
+
+const result = {
+  list : [studentObj]
+}
+console.log("result", result);
+
+
+
+    
+
